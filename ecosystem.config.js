5ff53f04847be623ce7b1605ddc438bb36de5ab7
Eduardo Ferreira <eduardo.ckf@gmail.com>
2020-12-04 08:38:10 -0300

    ecosystem for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dps.eckf.pw',
+      port: 3000,
+      script: './node_modules/nuxt/bin/nuxt-start',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
